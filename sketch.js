9fd006fd4c632091504eda05990eede706b0966f
bedantasarma <75606604+bedantasarma@users.noreply.github.com>
2021-05-10 13:46:32 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,68 +1,62 @@
-var fairy,fairyImg;
-var starImg,star;
-var bgImage,starBody,fairyVoice;
+var starImg, fairyImg, bgImg;
+var fairy , fairyVoice;
+var star, starBody;
 
-const World=Matter.World;
-const Engine=Matter.engine;
+const Engine = Matter.Engine;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
 function preload()
 {
-   //preload the images here
-  fairyImage=loadImage("fairy1.png");
-  fairyVoice=loadSound("JoyMusic.mp3");
-  bgImage=loadImage("starnight.png");
-  starImage=loadImage("star.png");
+	starImg = loadImage("star.png");
+	fairyImg = loadAnimation("fairyImage1.png","fairyImage2.png");
+	bgImg = loadImage("starNight.png");
+	fairyVoice = loadSound("JoyMusic.mp3");
 
 }
 
 function setup() {
-  createCanvas(800, 750);
+	createCanvas(800, 750);
 
-  fairy=createSprite(200,600);
-  fairy.addImage(fairyImage);
-  fairy.scale=0.25;
-  fairyVoice.play();
+	// fairyVoice.play();
 
-  star=createSprite(740,37);
-  star.addImage(starImage);
-  star.scale=0.3;
-  
-  engine=Engine.create();
-  world=engine.world;
+	fairy = createSprite(130, 520);
+	fairy.addAnimation("fairyflying",fairyImg);  
+	fairy.scale =0.25;
 
-  starBody=Bodies.circle(650,30,5,{restitution:0.5,isStatic:true});
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
 
-  World.add(world,starBody);
+	engine = Engine.create();
+	world = engine.world;
 
-  Engine.run(engine);
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
 
 }
-
 
 
 function draw() {
-  background(bgImage);
-
-  star.x=StarBody.position.x
-  star.y=StarBody.position.y
-
-  if(star.y>500 && starBody.position.y>500){
-    Matter.Body.setStatic(starbody,true);
-  }
+  background(bgImg);
 
   drawSprites();
-}
-function keyPressed() {
-  if(keyCode===LEFT_ARROW){
-    fairy.x=fairy.x-25;
-  }
-  if(keyCode===RIGHT_ARROW){
-    fairy.x=fairy.x+25;
-  }
 
- if (keyCode === DOWN_ARROW) {
-		Matter.Body.setStatic(starBody,false); 
+}
+
+function keyPressed() {
+	//write code here
+	if( keyCode===RIGHT_ARROW){
+		fairy.x=fairy.x+8;
+	}
+	if( keyCode===LEFT_ARROW){
+		fairy.x=fairy.x-8;
+	}
+	if( keyCode===DOWN_ARROW){
+		star.velocityY=4;
 	}
 }
+
